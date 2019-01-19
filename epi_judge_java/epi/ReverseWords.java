2b4d12836af2992f35099bbb5cb15230e625bda1
Marcos Hass Wakamatsu <marcoshass@gmail.com>
2019-01-19 17:44:23 -0200

    Reverse words completed

--- epi_judge_java/epi/ReverseWords.java
+++ epi_judge_java/epi/ReverseWords.java
@@ -1,28 +1,49 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 import epi.test_framework.TimedExecutor;
+
 public class ReverseWords {
 
-  public static void reverseWords(char[] input) {
-    // TODO - you fill in here.
-    return;
-  }
-  @EpiTest(testDataFile = "reverse_words.tsv")
-  public static String reverseWordsWrapper(TimedExecutor executor, String s)
-      throws Exception {
-    char[] sCopy = s.toCharArray();
+	public static void reverseWords(char[] input) {
+		int n = input.length;
+		reverse(input, 0, n - 1);
 
-    executor.run(() -> reverseWords(sCopy));
+		int start = 0, end = 0;
 
-    return String.valueOf(sCopy);
-  }
+		while (end < n) {
+			while (start < end || start < n && input[start] == ' ') {
+				start++;
+			}
+			while (end < start || end < n && input[end] != ' ') {
+				end++;
+			}
+			reverse(input, start, end - 1);
+		}
 
-  public static void main(String[] args) {
-    System.exit(
-        GenericTest
-            .runFromAnnotations(args, "ReverseWords.java",
-                                new Object() {}.getClass().getEnclosingClass())
-            .ordinal());
-  }
+		return;
+	}
+
+	static void reverse(char[] s, int start, int end) {
+		while (start < end) {
+			char tmp = s[start];
+			s[start++] = s[end];
+			s[end--] = tmp;
+		}
+	}
+
+	@EpiTest(testDataFile = "reverse_words.tsv")
+	public static String reverseWordsWrapper(TimedExecutor executor, String s) throws Exception {
+		char[] sCopy = s.toCharArray();
+
+		executor.run(() -> reverseWords(sCopy));
+
+		return String.valueOf(sCopy);
+	}
+
+	public static void main(String[] args) {
+		System.exit(GenericTest.runFromAnnotations(args, "ReverseWords.java", new Object() {
+		}.getClass().getEnclosingClass()).ordinal());
+	}
 }
