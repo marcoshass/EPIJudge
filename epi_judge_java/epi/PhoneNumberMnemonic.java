fd807e927895606bc02b88cd1d561e6fda376d48
Marcos Hass Wakamatsu <marcos@flexiblefunding.com>
2019-01-29 23:41:41 -0200

    - PhoneNumberMnemonic completed

--- epi_judge_java/epi/PhoneNumberMnemonic.java
+++ epi_judge_java/epi/PhoneNumberMnemonic.java
@@ -2,6 +2,8 @@ package epi;
 import epi.test_framework.EpiTest;
 import epi.test_framework.EpiTestComparator;
 import epi.test_framework.GenericTest;
+
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.function.BiPredicate;
@@ -9,9 +11,26 @@ public class PhoneNumberMnemonic {
   @EpiTest(testDataFile = "phone_number_mnemonic.tsv")
 
   public static List<String> phoneMnemonic(String phoneNumber) {
-    // TODO - you fill in here.
-    return null;
+	List<String> perms = new ArrayList<>();
+	permuteHelper(phoneNumber, 0, "", perms);
+    return perms;
   }
+  
+  private static final String[] MAPPINGS = {"0", "1", "ABC", "DEF", "GHI",
+		  "JKL", "MNO", "PQRS", "TUV", "WXYZ"};
+  
+  static void permuteHelper(String phoneNumber, int digit, String prefix, 
+		  List<String> permutations) {
+	  if (digit == phoneNumber.length()) {
+		  permutations.add(prefix);
+	  } else {
+		  String mnemonic = MAPPINGS[phoneNumber.charAt(digit) - '0'];
+		  for (int i = 0; i < mnemonic.length(); ++i) {
+			  permuteHelper(phoneNumber, digit + 1, prefix + mnemonic.charAt(i), permutations);
+		  }
+	  }
+  }
+  
   @EpiTestComparator
   public static BiPredicate<List<String>, List<String>> comp =
       (expected, result) -> {
